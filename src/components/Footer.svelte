976513cdabedfdb55b574cf0a8a3794060fd309b
vancerbtw <supremevance@gmail.com>
2023-07-26 11:02:47 -0500

    Update footer year

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,7 +2,7 @@
   <div class="sm:px-5 w-10/12 xl:w-2/3 flex flex-row items-start border-t-2 border-slate-800 py-8 dark:border-white/[.2] pb-20 gap-2">
     <div class="flex-1">
       <h1 class="text-black dark:text-white/50 text-2xl font-semibold">Vance McHargue</h1>
-      <h2 class="text-black/60 dark:text-white/30 text-base">Software Engineer • 2022</h2>
+      <h2 class="text-black/60 dark:text-white/30 text-base">Software Engineer • 2023</h2>
     </div>
 
     <div class="flex flex-1 flex-row items-end justify-end">
