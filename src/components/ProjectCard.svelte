3b46e8ad6adf3576073cce2a1d634d7fa52a8be6
vancerbtw <supremevance@gmail.com>
2023-07-26 10:48:26 -0500

    fix shadow not overflowing on project card

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -15,45 +15,46 @@
   }
 </script>
 
-<div 
-  class="
-    project-card
-    bg-white
-    drop-shadow-md
-    dark:bg-[#414146]
-    justify-self-center
-    flex
-    flex-col
-    rounded-xl
-    overflow-hidden
-  "
->
-  <img src={bannerUrl} alt={`${name} Banner`} class="
-    w-full
-    object-cover
-  " />
-
-  <div class="py-2 flex items-center px-4">
-    <img src={iconUrl} alt={`${name} Icon`} class="
-      w-10 h-10
-      lg:w-14 lg:h-14
-      rounded-full
+<div class="drop-shadow-md">
+  <div 
+    class="
+      project-card
+      bg-white
+      dark:bg-[#414146]
+      justify-self-center
+      flex
+      flex-col
+      rounded-xl
+      overflow-hidden
+    "
+  >
+    <img src={bannerUrl} alt={`${name} Banner`} class="
+      w-full
+      object-cover
     " />
 
-    <p class="text-2xl font-semibold ml-4">{name}</p>
+    <div class="py-2 flex items-center px-4">
+      <img src={iconUrl} alt={`${name} Icon`} class="
+        w-10 h-10
+        lg:w-14 lg:h-14
+        rounded-full
+      " />
 
-    <div class="flex-1 flex justify-end">
-      <button
-        on:click={visit}
-        class="
-          p-2 rounded-xl px-8
-          hover:scale-110 transition in-expo cursor-pointer transition
-          text-white font-semibold
-        "
-        style="background: {buttonBackground};"
-      >
-        Visit
-      </button>
+      <p class="text-2xl font-semibold ml-4">{name}</p>
+
+      <div class="flex-1 flex justify-end">
+        <button
+          on:click={visit}
+          class="
+            p-2 rounded-xl px-8
+            hover:scale-110 transition in-expo cursor-pointer transition
+            text-white font-semibold
+          "
+          style="background: {buttonBackground};"
+        >
+          Visit
+        </button>
+      </div>
     </div>
   </div>
 </div>
--- src/components/Projects.svelte
+++ src/components/Projects.svelte
@@ -8,7 +8,7 @@
   <div class="sm:px-5 w-10/12 xl:w-2/3 py-8 pb-20">
     <h1 class="text-4xl font-semibold">My Projects</h1>
 
-    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-6">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-6 overflow-visible">
       <ProjectCard projectUrl="https://revvy.dev/" name="Revvy" iconUrl={RevvyIconUrl} bannerUrl={RevvyBannerUrl} buttonBackgroundProp="linear-gradient(95.99deg, #3E2ACB 7.76%, #5A1DB6 71.74%)" />
     </div>
   </div>
